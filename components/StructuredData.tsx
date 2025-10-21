921f5460b5ea24c9054324ea6656d29a69030de9
anhaa-arch <anhaaidiot@gmail.com>
2025-10-21 21:10:33 +0800

    Fix: Use Next.js Script component for structured data

--- components/StructuredData.tsx
+++ components/StructuredData.tsx
@@ -1,17 +1,21 @@
+import Script from 'next/script'
 import { personSchema, websiteSchema, professionalServiceSchema } from '@/lib/structured-data'
 
 export default function StructuredData() {
   return (
     <>
-      <script
+      <Script
+        id="structured-data-person"
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(personSchema) }}
       />
-      <script
+      <Script
+        id="structured-data-website"
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
       />
-      <script
+      <Script
+        id="structured-data-service"
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(professionalServiceSchema) }}
       />
